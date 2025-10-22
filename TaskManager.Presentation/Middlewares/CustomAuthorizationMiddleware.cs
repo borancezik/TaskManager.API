837b79836169a02319a65b46cf4f7911e093efba
Boran Cezik <boran.cezik@affinitybox.com>
2025-10-22 11:39:37 +0300

    Add CustomAuthorizationMiddleware for request handling
    Introduce `CustomAuthorizationMiddleware` to manage custom
    authorization logic in the `TaskManager.Presentation.Middlewares`
    namespace. The middleware uses `IOptions<TaskManagerSettings>`
    to access app settings and checks for `AllowAnonymousAttribute`
    metadata to determine if authorization should be bypassed.
    
    Includes placeholder for additional authorization logic.

---
+++ TaskManager.Presentation/Middlewares/CustomAuthorizationMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using TaskManager.Application.Utilities.AppSettings;
+
+namespace TaskManager.Presentation.Middlewares;
+
+public class CustomAuthorizationMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    private readonly IOptions<TaskManagerSettings> _appSettings;
+
+    public CustomAuthorizationMiddleware(IOptions<TaskManagerSettings> appSettings, RequestDelegate next)
+    {
+        _appSettings = appSettings;
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext httpContext)
+    {
+        var endpoint = httpContext.GetEndpoint();
+
+        var hasAuhtIgnore = endpoint.Metadata.OfType<AllowAnonymousAttribute>().ToList();
+
+        if (!hasAuhtIgnore.Any())
+        {
+
+        }
+
+        await _next(httpContext);
+    }
+}
